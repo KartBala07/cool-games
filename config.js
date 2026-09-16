8dfddc73318ef182b85174b0e347770af866f92a
KartBala07 <kartbala2009@gmail.com>
2026-09-15 18:34:25 -0700

    Add config file for Instagram API key

---
+++ config.js
@@ -0,0 +1,18 @@
+// Configuration for Cool Games
+// Add your Instagram API credentials here
+
+const CONFIG = {
+  // Instagram API Configuration
+  instagram: {
+    // Get your Instagram Business Account ID from: https://developers.facebook.com/
+    // This is your Instagram Business Account ID
+    businessAccountId: 'YOUR_INSTAGRAM_BUSINESS_ACCOUNT_ID',
+    
+    // Get your Access Token from Meta for Developers:
+    // https://developers.facebook.com/docs/instagram-graph-api/getting-started
+    accessToken: 'YOUR_INSTAGRAM_ACCESS_TOKEN',
+    
+    // Your Instagram username (for fallback display)
+    username: 'YOUR_INSTAGRAM_USERNAME'
+  }
+};
